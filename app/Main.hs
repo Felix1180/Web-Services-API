e147dcfbbf46ae88049991fb55c6202792291721
Felix1180 <134295039+Felix1180@users.noreply.github.com>
2024-12-12 23:27:33 +0800

    Nih Guys

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,9 @@
 
 module Main where
 
-import Web.Scotty 
+import Web.Scotty
 import Sentiment (analyzeSentiment)
+import Network.Wai.Middleware.Cors (cors, simpleCors)
 import WordCounter (wordCount, charCount)
 import StopWords (removeStopwords)
 import qualified Data.Text.Lazy as T
@@ -12,10 +13,13 @@ import Data.Aeson (object, (.=))
 
 main :: IO ()
 main = scotty 3000 $ do
+    -- Enable CORS for all routes
+    middleware simpleCors
+
     get "/" $ text "Welcome to Text Analysis API!"
     
     post "/analyze" $ do
-        maybeInput <- formParam "text" `catch` (\ (_ :: ScottyException) -> return "")
+        maybeInput <- formParam "text" `catch` (\ (_ :: ScottyException) -> return T.empty)
         liftIO $ putStrLn "Processing request..."
         liftIO $ print ("Received text: " ++ show maybeInput)
         if T.null maybeInput
@@ -31,7 +35,3 @@ main = scotty 3000 $ do
                     , "char_count" .= cc
                     , "text_no_stopwords" .= noStopwords
                     ]
-
-
-
-
